2b5cd677b01c87a589564b840cbbe54f42c5f291
brunodavi <48166556+brunodavi@users.noreply.github.com>
2025-11-20 17:16:45 -0300

    feat: MissaoModel usando lombok para adaptação de construtores e getters e setters

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -2,13 +2,18 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissaoModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -21,57 +26,4 @@ public class MissaoModel {
     // Uma missão para muitos ninjas
     @OneToMany(mappedBy = "missao")
     private List<NinjaModel> ninjasAtribuidos;
-
-    public MissaoModel() {
-    }
-
-    public MissaoModel(String titulo, String descricao, MissaoNivelEnum missaoNivelEnum) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.missaoNivelEnum = missaoNivelEnum;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public boolean isMissaoConcluida() {
-        return missaoConcluida;
-    }
-
-    public void setMissaoConcluida(boolean missaoConcluida) {
-        this.missaoConcluida = missaoConcluida;
-    }
-
-    public MissaoNivelEnum getMissaoNivelEnum() {
-        return missaoNivelEnum;
-    }
-
-    public void setMissaoNivelEnum(MissaoNivelEnum missaoNivelEnum) {
-        this.missaoNivelEnum = missaoNivelEnum;
-    }
-
-    public List<NinjaModel> getNinjasAtribuidos() {
-        return ninjasAtribuidos;
-    }
-
-    public void setNinjasAtribuidos(List<NinjaModel> ninjasAtribuidos) {
-        this.ninjasAtribuidos = ninjasAtribuidos;
-    }
 }
