11ecf937d5eba99df46f87fba205ab974d8d76b4
brunodavi <48166556+brunodavi@users.noreply.github.com>
2025-11-20 16:40:43 -0300

    Criação inicial do cadastro de missões

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoController.java
@@ -0,0 +1,41 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/missao")
+public class MissaoController {
+
+    @GetMapping
+    public List<MissaoModel> listarMissoes() {
+        return List.of();
+    }
+
+    @GetMapping
+    public MissaoModel obterMissao(long id) {
+        return new MissaoModel();
+    }
+
+    @PostMapping
+    public MissaoModel criarMissao() {
+        return new MissaoModel();
+    }
+
+    @PutMapping
+    public MissaoModel atualizarMissao(long id, MissaoModel missaoModel) {
+        return missaoModel;
+    }
+
+    @DeleteMapping
+    public MissaoModel removerMissao(long id) {
+        return new MissaoModel();
+    }
+
+    @PostMapping
+    public List<NinjaModel> atribuirNinjas(long missaoId, long[] ninjaId) {
+        return List.of();
+    }
+}
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoNivelEnum.java
@@ -0,0 +1,10 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+public enum MissaoNivelEnum {
+    S,
+    A,
+    B,
+    C,
+    D,
+    E;
+}
